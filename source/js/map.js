15930ca00eb22c7a70c1f71c48776a278b455e7b
Nazzarik <nazzarik@gmail.com>
2018-03-06 21:03:39 +0200

    add file Gulp

--- source/js/map.js
+++ source/js/map.js
@@ -4,27 +4,27 @@ var mapPopup = document.querySelector(".modal-map");
 var mapClose = document.querySelector(".modal-close-map");
 
 mapLink.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	mapPopup.classList.add("modal-show");
-	console.log("відкрив");
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+  console.log("відкрив");
 });
 
 mapLinkFooter.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	mapPopup.classList.add("modal-show");
-	console.log("відкрив");
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+  console.log("відкрив");
 });
 
 mapClose.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	mapPopup.classList.remove("modal-show");
-	console.log("закрив");
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+  console.log("закрив");
 });
 
 window.addEventListener("keydown", function(evt) {
-	if (evt.keyCode === 27) {
-		if (mapPopup.classList.contains("modal-show")) {
-			mapPopup.classList.remove("modal-show");
-		}
-	}
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
 });
--- source/js/popup.js
+++ source/js/popup.js
@@ -8,36 +8,36 @@ var password = popup.querySelector("[name=password]");
 var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.add("modal-show");
-	if (storage) {
-		login.value = storage;
-		password.focus();
-	} else {
-		login.focus();
-	}
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
 });
 
 close.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.remove("modal-show");
-	popup.classList.remove("modal-error");
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
-	if (!login.value || !password.value) {
-		evt.preventDefault();
-		popup.classList.add("modal-error");
-		console.log("Введіть дані");
-	} else {
-		localStorage.setItem("login", login.value);
-	}
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    console.log("Введіть дані");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
 });
 
 window.addEventListener("keydown", function(evt) {
-	if (evt.keyCode === 27) {
-		if (popup.classList.contains("modal-show")) {
-			popup.classList.remove("modal-show");
-		}
-	}
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
 });
