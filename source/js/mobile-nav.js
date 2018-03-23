beb54c6e515520f2ec9a2c4e9fa2bddec3cc8461
Nazzarik <nazzarik@gmail.com>
2018-03-23 23:45:29 +0200

    add adaptive grafic index.html & photo.html

--- source/js/mobile-nav.js
+++ source/js/mobile-nav.js
@@ -1,6 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
-
+// console.log('Hello');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
