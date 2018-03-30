4f12a769e45ddde17363ade36e82df941e26d10d
Nazzarik <nazzarik@gmail.com>
2018-03-30 12:42:49 +0300

    adds gulp assembly

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,90 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var del = require("del");
+var run = require("run-sequence");
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var svgstore = require("gulp-svgstore");
+
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
+var normalize = require("node-normalize-scss");
+
+/////////////////////////////////////////////////////
+gulp.task("delate", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**/*.js"], {
+      base: "./source/"
+    })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("build/img/icon-*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+    ]))
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+  run("delate", "copy", "style", "sprite", "html", done);
+});
+
+gulp.task("image", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+    ]))
+  .pipe(gulp.dest("build/img/min-img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img/webp"));
+});
+/////////////////////////////////////////////////////
+
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
     .pipe(plumber())
-    .pipe(sass())
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style-min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -28,5 +98,5 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
